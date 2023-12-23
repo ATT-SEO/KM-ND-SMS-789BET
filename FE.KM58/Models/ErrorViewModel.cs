f5ec1a2ab1c036ba0a7230d1d4d036aaf91f7c4b
kawaitcn <kawaitcn@gmail.com>
2023-12-23 13:56:17 +0700

    push fe init

---
+++ FE.KM58/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FE.KM58.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
