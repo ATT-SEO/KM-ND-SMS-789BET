6f20707a445bcd047ac31af2e39ee7f9fba74443
Kawa <kawaitcn@gmail.com>
2024-01-18 13:12:10 +0700

    add auth module

--- FE.ADMIN/Services/IService/IAuthService.cs
+++ FE.ADMIN/Services/IService/IAuthService.cs
@@ -4,11 +4,8 @@ namespace FE.ADMIN.Services.IService
 {
     public interface IAuthService
     {
-        Task<ResponseDTO> GetAsync();
-        Task<ResponseDTO> LoginAsync(LoginRequestDTO requestDTO);
-        Task<ResponseDTO> RegisterAsync(RegistrationRequestDTO requestDTO);
-        Task<ResponseDTO> AssignRoleAsync(RegistrationRequestDTO requestDTO);
-        Task<ResponseDTO> DeleteAsync(string IdentityUser);
-
-	}
+        Task<ResponseDTO?> LoginAsync(LoginRequestDTO requestDTO);
+        Task<ResponseDTO?> RegisterAsync(RegistrationRequestDTO requestDTO);
+        Task<ResponseDTO?> AssignRoleAsync(RegistrationRequestDTO requestDTO);
+    }
 }
