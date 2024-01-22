f10330aad38ed9ef5fd08a43346672d7f7a652c2
Kawa <kawaitcn@gmail.com>
2024-01-22 19:07:42 +0700

    update mantaince screen for CMD

--- FE.JunCMD.Client/Controllers/HomeController.cs
+++ FE.JunCMD.Client/Controllers/HomeController.cs
@@ -1,19 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FE.JunCMD.Client.Models;
+using FE.JunCMD.Client.Services.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace FE.JunCMD.Client.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ISiteService _siteService;
+        public HomeController(ISiteService siteService)
         {
-            _logger = logger;
+            _siteService = siteService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ResponseDTO _res = await _siteService.GetByProjectCode("K58");
+            Console.WriteLine(JsonConvert.SerializeObject(_res));
+            if (_res != null && _res.IsSuccess)
+            {
+                List<SiteDTO> _siteService = JsonConvert.DeserializeObject<List<SiteDTO>>(_res.Result.ToString());
+                for (int i = 0; i < _siteService.Count; i++)
+                {
+                    if (_siteService[i].Status == false)
+                    {
+                        return Redirect("https://att-maintenance.web.app/");
+                    }
+                }
+            }
+
             ViewData["Title"] = "Jun88 - Khuyến mãi 58k";
             ViewData["Description"] = "Chương trình khuyến mãi của Jun88 dành cho thành viên Jun88 - Khuyến mãi 58k ";
             ViewData["Keywords"] = "Jun88, khuyến mãi, khuyến mãi 58k, đăng ký, hội viên ...";
